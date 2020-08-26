a0dec1b8442d6530b88195ed830e088320f4e496
iamuttamsharma15 <iamuttamsharma15@gmail.com>
2020-08-27 01:27:24 +0530

    Add environment veriable

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const mongoose =require("mongoose");
+
 
 const encrypt=require("mongoose-encryption");
 
@@ -8,7 +10,7 @@ var userSchema=new mongoose.Schema({
     email:String,
     password:String
 })
-var secret="thisisonlysecretkey";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 module.exports=mongoose.model("user",userSchema);
